d3d2a5dbea00cadc9aea32108d6128375939f43f
Tyler Tedesco <s_tylert@icloud.com>
2022-07-11 19:51:08 -0600

    Update profile-selection-spec.cy.js

--- cypress/e2e/profile-selection-spec.cy.js
+++ cypress/e2e/profile-selection-spec.cy.js
@@ -1,7 +1,7 @@
 
 describe('swolify-fe', () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000")
+    cy.visit("http://localhost:3000/")
   })
 
   it('Should contain the Swolify title', () => {
