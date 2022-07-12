344301b627d59e421bce4ce0986792a3d1973d2b
Stephen Tedesco <s_tylert@icloud.com>
2022-07-12 13:29:18 -0600

    Add link back to user profile button

--- src/Components/UserIcon.js
+++ src/Components/UserIcon.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../Styles/UserIcon.css'
+import { Link } from 'react-router-dom'
 
 export const UserIcon = ({ image }) => {
 
   return (
     <div className='user-icon-background'>
-        <img className='user-icon' src={image} />
+        <Link to='/dashboard'> <img className='user-icon' src={image} /> </Link>
     </div>
   )
 }
