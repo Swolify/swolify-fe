843bd0116e1a43ee6e5c5b8b35073261abea61e9
Kevin King <kevinturingking@gmail.com>
2022-07-20 17:21:38 -0600

    Create README and get rid of some code not being used

--- src/Components/UserIcon.js
+++ src/Components/UserIcon.js
@@ -3,13 +3,12 @@ import '../Styles/UserIcon.css'
 import { Link } from 'react-router-dom'
 import { gql } from '@apollo/client';
 
-export const UserIcon = ({ image, id, setUserId, startTimer }) => {
+export const UserIcon = ({ image, id, setUserId }) => {
 
   return (
     <div className='user-icon-background'>
         <Link to='/game' > <img onClick={() =>  {
           setUserId(id)
-          startTimer()
           }} className='user-icon' src={image} /> </Link>
     </div>
   )
